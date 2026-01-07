afbc189b2df6b15ab326db0585f7ffa9d7cff12f
ameliayengo <ameliayengo180@gmail.com>
2026-01-07 11:06:10 +0100

    nvl version

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Animation simple au clic
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    alert("Section Projets en cours de construction 🚀");
+});
+
+
+
